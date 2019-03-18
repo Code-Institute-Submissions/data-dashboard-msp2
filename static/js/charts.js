8e5b7468d119faee463e4b1add807217c470423e
Daniel van Duinkerken <daanivd@hotmail.com>
2019-03-18 15:40:48 +0000

    Added stateChart and speciesSelect-menu, general text and images

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,6 +1,55 @@
 queue()
-  .defer(d3.csv, "data/monotremeDF.csv")
+  .defer(d3.csv, "/data/monotremeDF.csv")
   .await(makeGraphs);
 
+/*
+d3.csv("/data/monotremeDF.csv").then(function(data) {
+    console.log(data)
+});*/
+
+
 function makeGraphs(error, sightingsData) {
+    
+    //CREATE CROSSFILTER RECORD
+ 
+    
+
+    var ndx = crossfilter(sightingsData);
+
+//CREATE DIMENSIONS
+    var speciesDim = ndx.dimension(dc.pluck('vernacularName'));
+    var stateDim = ndx.dimension(dc.pluck('stateProvince'));
+
+    //CREATE GROUPS 
+    var speciesGroup = speciesDim.group();
+    var sightingsStateGroup = stateDim.group()
+
+    //DC.JS Charts
+    var selectSpecies = dc.selectMenu("#selectSpecies");
+    var stateChart = dc.rowChart("#sightings_per_state");
+
+
+    //CHART PROPERTIES
+    selectSpecies
+        .dimension(speciesDim)
+        .group(speciesGroup)
+        .controlsUseVisibility(true);
+
+    stateChart
+        .width(600)
+        .height(555)
+        .dimension(stateDim)
+        .group(sightingsStateGroup)
+        .colors(['#f5f5f5'])
+        .elasticX(true)
+        .labelOffsetY(10)
+        .xAxis().ticks(4);
+
+
+
+    //RENDER CHARTS
+    dc.renderAll(); 
+   
 }
+
+
